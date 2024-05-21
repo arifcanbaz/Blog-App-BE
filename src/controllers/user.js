6e00e4653cd6039ad34728ef32c2477a23c0d4c3
arifcanbaz <arifcanbaz908@gmail.com>
2024-05-21 22:28:04 +0300

    user model controller

---
+++ src/controllers/user.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const User = require ('../models/user')
+
+module.exports = {
+    list: async (req, res) => {
+
+        const data = await res.getModelList(User)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(User),
+            data
+        })
+    },
+
+    create: async (req, res) => {
+        const data = await User.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+
+    read: async (req,res) => {
+        const data = await User.findOne({ _id: req.params.id})
+
+        res.status(201).send({
+            error:false,
+            data
+        })
+    },
+
+    update: async (req, res) => {
+        const data = await User.updateOne( {_id: req.params.id}, req.body, {runValidators:true})
+    },
+    
+}
