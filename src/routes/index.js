732c743eda6583b584e49fcbf4e0c3e1c0d10fd2
arifcanbaz <arifcanbaz908@gmail.com>
2024-05-22 20:59:11 +0300

    blogs

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,6 @@ router.use("/users",require("./user"))
 router.use("/tokens",require("./token"))
 router.use("/auth",require("./auth"))
 router.use("/categories",require("./category"))
+router.use("/blogs",require("./blogs"))
 
 module.exports = router
